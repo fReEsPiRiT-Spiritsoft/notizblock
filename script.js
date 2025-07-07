75c0a1c02d2e82a3b50e85215ecfb4bbb155ba6d
fReEsPiRiT-Spiritsoft <p.schmidt.391994@gmail.com>
2025-07-07 14:22:14 +0200

    Add and delete functions implemented with titles

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let notesTitles = [
+    "titel 1",
+    "titel 2",
+    "titel 3"
+];
 //notizen anziegen lassen
 let notes = [
     "beispiel notiz 1",
@@ -5,26 +10,45 @@ let notes = [
     "beispiel notiz 3",
 ];
 
+let trashNotesTitles = [];
+let trashNotes = [];
 
-function renderNotes(){
+
+function renderNotes() {
 
     let contentRef = document.getElementById("content");
-    contentRef.innerHTML = ''; 
+    contentRef.innerHTML = '';
     for (let indexNote = 0; indexNote < notes.length; indexNote++) {
-        const note = notes[indexNote];
-        
+        // const note = notes[indexNote];
+
         contentRef.innerHTML += getNoteTemplate(indexNote);
     }
 }
 
+function renderTrashNotes() {
 
-function getNoteTemplate(indexNote){
-    return `<p>+ ${notes[indexNote]}<button onclick="deleteNote(${indexNote})">x</button></p>`
+    let trashContentRef = document.getElementById("trash_content");
+    trashContentRef.innerHTML = '';
+    for (let indexTrashNote = 0; indexTrashNote < trashNotes.length; indexTrashNote++) {
+        // const note = notes[indexTrashNote];
+
+        trashContentRef.innerHTML += getTrashNoteTemplate(indexTrashNote);
+    }
+}
+
+
+function getNoteTemplate(indexNote) {
+    return `<p>+ Titel: ${notesTitles[indexNote]} <br> Notiz: ${notes[indexNote]}<button onclick="moveToTrash(${indexNote})">x</button><br><br></p>`
+}
+
+
+function getTrashNoteTemplate(indexTrashNote) {
+    return `<p>+ Titel: ${trashNotesTitles[indexTrashNote]} <br> Notiz:  ${trashNotes[indexTrashNote]}<button onclick="moveToTrash(${indexTrashNote})">x</button><br><br></p>`
 }
 
 
 //notizen hinzufügen
-function addNote(){
+function addNote() {
     let noteInputRef = document.getElementById("noteInput");
     let noteInput = noteInputRef.value;
     notes.push(noteInput);
@@ -34,10 +58,24 @@ function addNote(){
 }
 
 
-//notizen löschen
-function deleteNote(indexNote){
-    notes.splice(indexNote, 1);
+//in mülleimer verschieben
+function moveToTrash(indexNote) {
+    let trashNote = notes.splice(indexNote, 1);
+    trashNotes.push(trashNote[0]);
+    let trashNoteTitle = notesTitles.splice(indexNote, 1);
+    trashNotesTitles.push(trashNoteTitle[0]);
+    renderNotes();
+    renderTrashNotes();
 }
+
+
+//notiz löschen
+function deleteNote(indexTrashNote) {
+    trashNotes.splice(indexTrashNote, 1);
+    renderNotes();
+    renderTrashNotes();
+}
+
 
 // notizen archivieren
 // notizen bearbeiten
